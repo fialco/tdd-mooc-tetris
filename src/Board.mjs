c498b1ace4aa729475adc464ff6cc21a3a331976
Fialco <113672867+Fialco@users.noreply.github.com>
2026-01-09 16:08:49 +0200

    Refactor: Privatize MovableShape col

--- src/Board.mjs
+++ src/Board.mjs
@@ -16,22 +16,22 @@ class Point {
 class MovableShape {
   #shape;
   #row;
-  col;
+  #col;
 
   constructor(shape, row, col) {
     this.#shape = shape;
     this.#row = row;
-    this.col = col;
+    this.#col = col;
   }
 
   moveDown() {
-    return new MovableShape(this.#shape, this.#row + 1, this.col);
+    return new MovableShape(this.#shape, this.#row + 1, this.#col);
   }
 
   nonEmptyBlocks() {
     const points = [];
     for (let row = this.#row; row < this.#row + this.#shape.height(); row++) {
-      for (let col = this.col; col < this.col + this.#shape.width(); col++) {
+      for (let col = this.#col; col < this.#col + this.#shape.width(); col++) {
         const block = this.blockAt(row, col);
         if (block !== EMPTY) {
           points.push(new Point(row, col));
@@ -45,10 +45,10 @@ class MovableShape {
     if (
       row >= this.#row &&
       row < this.#row + this.#shape.height() &&
-      col >= this.col &&
-      col < this.col + this.#shape.width()
+      col >= this.#col &&
+      col < this.#col + this.#shape.width()
     ) {
-      return this.#shape.blockAt(row - this.#row, col - this.col);
+      return this.#shape.blockAt(row - this.#row, col - this.#col);
     } else {
       return EMPTY;
     }
