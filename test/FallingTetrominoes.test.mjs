0e0b828830d27d5a341555afc8102e990c800888
Fialco <113672867+Fialco@users.noreply.github.com>
2026-01-11 16:02:50 +0200

    Level 5: Add failing move down test

--- test/FallingTetrominoes.test.mjs
+++ test/FallingTetrominoes.test.mjs
@@ -85,4 +85,18 @@ describe("Falling tetrominoes", () => {
        ..........`
     );
   });
+
+  test("can be moved down", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveDown();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ....T.....
+       ...TTT....
+       ..........
+       ..........
+       ..........`
+    );
+  });
 });
