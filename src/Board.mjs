94d0ab84337e2e5db7f5e73201214f13e59e5bc6
Fialco <113672867+Fialco@users.noreply.github.com>
2026-04-15 22:02:30 +0300

    Add function to drop all shapes from the top

--- src/Board.mjs
+++ src/Board.mjs
@@ -100,7 +100,19 @@ export class Board {
     if (typeof piece === "string") {
       piece = new Block(piece);
     }
-    this.#falling = new MovableShape(piece, 0, Math.floor((this.#width - piece.width()) / 2));
+    this.#falling = new MovableShape(
+      piece,
+      -this.#firstNonEmptyRow(piece),
+      Math.floor((this.#width - piece.width()) / 2)
+    );
+  }
+
+  #firstNonEmptyRow(piece) {
+    for (let row = 0; row < piece.height(); row++) {
+      for (let col = 0; col < piece.width(); col++) {
+        if (piece.blockAt(row, col) !== EMPTY) return row;
+      }
+    }
   }
 
   tick() {
