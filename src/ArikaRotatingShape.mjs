1a4d0cbb07da7aff5e584bdffd86b921ebe43e12
Fialco <113672867+Fialco@users.noreply.github.com>
2026-04-22 19:09:42 +0300

    Level 7: Create copies of files for Arika system

---
+++ src/ArikaRotatingShape.mjs
@@ -0,0 +1,52 @@
+import { shapeToString } from "./shapes.mjs";
+
+function newSquareArray(size) {
+  const array = new Array(size);
+  for (let row = 0; row < size; row++) {
+    array[row] = new Array(size);
+  }
+  return array;
+}
+
+export class RotatingShape {
+  #shape;
+
+  constructor(shape, row, col) {
+    this.#shape = shape;
+  }
+
+  static fromString(shape) {
+    return new RotatingShape(shape.replaceAll(" ", "").split("\n"));
+  }
+
+  width() {
+    return this.#shape[0].length;
+  }
+
+  height() {
+    return this.#shape.length;
+  }
+
+  blockAt(row, col) {
+    return this.#shape[row][col];
+  }
+
+  rotateRight() {
+    const size = this.#shape.length;
+    const rotated = newSquareArray(size);
+    for (let row = 0; row < size; row++) {
+      for (let column = 0; column < size; column++) {
+        rotated[row][column] = this.#shape[size - 1 - column][row];
+      }
+    }
+    return new RotatingShape(rotated);
+  }
+
+  rotateLeft() {
+    return this.rotateRight().rotateRight().rotateRight();
+  }
+
+  toString() {
+    return shapeToString(this);
+  }
+}
---
+++ src/ArikaTetromino.mjs
@@ -0,0 +1,76 @@
+import { RotatingShape } from "./RotatingShape.mjs";
+
+export class Tetromino {
+  #currentOrientation;
+  #orientations;
+
+  constructor(currentOrientation, orientations) {
+    this.#currentOrientation = (currentOrientation + orientations.length) % orientations.length;
+    this.#orientations = orientations;
+  }
+
+  static T_SHAPE = Tetromino.fromString(
+    0,
+    4,
+    `.T.
+       TTT
+       ...`
+  );
+
+  static I_SHAPE = Tetromino.fromString(
+    0,
+    2,
+    `.....
+       .....
+       IIII.
+       .....
+       .....`
+  );
+
+  static O_SHAPE = Tetromino.fromString(
+    0,
+    1,
+    `.OO
+       .OO
+       ...`
+  );
+
+  static fromString(currentOrientation, orientationCount, initialShape) {
+    const shape = RotatingShape.fromString(initialShape);
+    const orientations = [
+      shape,
+      shape.rotateRight(),
+      shape.rotateRight().rotateRight(),
+      shape.rotateRight().rotateRight().rotateRight(),
+    ].slice(0, orientationCount);
+    return new Tetromino(currentOrientation, orientations);
+  }
+
+  rotateRight() {
+    return new Tetromino(this.#currentOrientation + 1, this.#orientations);
+  }
+
+  rotateLeft() {
+    return new Tetromino(this.#currentOrientation - 1, this.#orientations);
+  }
+
+  #shape() {
+    return this.#orientations[this.#currentOrientation];
+  }
+
+  width() {
+    return this.#shape().width();
+  }
+
+  height() {
+    return this.#shape().height();
+  }
+
+  blockAt(row, col) {
+    return this.#shape().blockAt(row, col);
+  }
+
+  toString() {
+    return this.#shape().toString();
+  }
+}
