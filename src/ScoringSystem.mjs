de7f73dfe0a076c2527f06ccabb0baf84f16a181
Fialco <113672867+Fialco@users.noreply.github.com>
2026-04-27 10:09:28 +0300

    L9: Add ScoringSystem class

---
+++ src/ScoringSystem.mjs
@@ -0,0 +1,16 @@
+// Using Original Nintendo scoring system
+// More on: https://tetris.wiki/Scoring
+
+export class ScoringSystem {
+  #score;
+  #level;
+
+  constructor() {
+    this.#score = 0;
+    this.#level = 0;
+  }
+
+  get score() {
+    return this.#score;
+  }
+}
