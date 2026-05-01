a341ce4494190f31f2a3a4f29a79aa78e9d103f8
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-01 14:30:37 +0300

    L10: Add first ShuffleBag test and implementation

---
+++ src/ShuffleBag.mjs
@@ -0,0 +1,5 @@
+export class ShuffleBag {
+  constructor(items) {
+    this.bag = [...items];
+  }
+}
---
+++ test/ShuffleBag.test.mjs
@@ -0,0 +1,16 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { ShuffleBag } from "../src/ShuffleBag.mjs";
+
+describe("Shuffle bag", () => {
+  let bag;
+  let items = ["I", "T", "L", "J", "T", "S", "Z", "O"];
+
+  beforeEach(() => {
+    bag = new ShuffleBag(items);
+  });
+
+  test("can hold items", () => {
+    expect(Object.keys(bag.bag).length).to.equal(items.length);
+  });
+});
