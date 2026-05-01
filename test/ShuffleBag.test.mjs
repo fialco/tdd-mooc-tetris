636115d6fb7dd203efd6e5afdee525e1d65e5de4
Fialco <113672867+Fialco@users.noreply.github.com>
2026-05-01 14:55:22 +0300

    L10: Test for bag refill

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -2,6 +2,12 @@ import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { ShuffleBag } from "../src/ShuffleBag.mjs";
 
+function pullItems(bag, count) {
+  for (let i = 0; i < count; i++) {
+    bag.next();
+  }
+}
+
 describe("Shuffle bag", () => {
   let bag;
   let items = ["I", "T", "L", "J", "T", "S", "Z", "O"];
@@ -15,6 +21,15 @@ describe("Shuffle bag", () => {
   });
 
   test("items can be pulled", () => {
+    bag.next();
+    expect(bag.itemCount).to.equal(items.length - 1);
+    bag.next();
+    expect(bag.itemCount).to.equal(items.length - 2);
+  });
+
+  test("refills after pulling all items", () => {
+    pullItems(bag, 8);
+    expect(bag.itemCount).to.equal(items.length - 8);
     bag.next();
     expect(bag.itemCount).to.equal(items.length - 1);
   });
